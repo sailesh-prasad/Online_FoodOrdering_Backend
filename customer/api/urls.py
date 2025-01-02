65a209617f4411d84a737f48271be52323377777
Your New Username <sankarsailesh001@gmail.com>
2025-01-02 20:20:32 +0530

    Backend Updated

--- customer/api/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from customer.api.views import CustomerUserViewSet
-
-router = DefaultRouter()
-router.register(r'customerusers', CustomerUserViewSet, basename='customeruser_unique3')  # Ensure basenames are unique
-
-urlpatterns = router.urls
